9aadf68197671e5ad03c6205277ef6570ac6e018
d-e-k-k <davideberhart8@gmail.com>
2020-12-11 11:20:43 -0500

    Revert "Created app urls"
    This reverts commit a9960bba5a7b70d73e34767813055d98c093fd08.

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from djoser.serializers import UserCreateSerializer, UserSerializer
-from rest_framework import serializers
-from . import models
-
-
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = models.User
-        fields = ('id', 'email', 'username', 'password', 'avatar')
