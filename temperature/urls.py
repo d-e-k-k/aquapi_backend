32249a2266314c6c057e9f227634ba93ed18444e
d-e-k-k <davideberhart8@gmail.com>
2020-12-14 15:08:38 -0500

    Updated permissions

--- temperature/urls.py
+++ temperature/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('temperatures/', views.temperature_list),
-    path('temperatures/<int:pk>', views.temperature_detail)
+    path('temperatures/<int:pk>/', views.temperature_detail),
+    path('temperatures/today/', views.temperature_list_date_range),
 ]
-    # path('temperatures/', views.Temperature_list.as_view())
+# path('temperatures/', views.Temperature_list.as_view())
