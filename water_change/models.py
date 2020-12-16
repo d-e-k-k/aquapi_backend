edd467a1c76d8a4550849ce95862db00ef6676ba
d-e-k-k <davideberhart8@gmail.com>
2020-12-16 08:27:09 -0500

    Added another model field

--- water_change/models.py
+++ water_change/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class WaterChange(model.Models):
     water_change_date = models.DateField(auto_now_add=True, auto_now=False)
-    water_change_interval = models.PositiveSmallIntegerField()
     time_stamp = models.DateField(auto_now_add=True, auto_now=False)
-
+    completed = models.BooleanField()
+    water_change_interval = models.PositiveSmallIntegerField()
     def __str__(self):
         return f'Last Water Change was on: {water_change_date}  Interval: {water_change_interval} days'
